25ffdb54273829fadbfb9295fce91715a24a5f5f
beraldoLucca <lucca.beraldo@aluno.ifsp.edu.br>
2025-05-21 21:37:30 -0300

    add unit tests

--- src/test/java/br/com/fiap/fast_food_kitchen/controller/KitchenControllerTest.java
+++ src/test/java/br/com/fiap/fast_food_kitchen/controller/KitchenControllerTest.java
@@ -1,17 +1,22 @@
 package br.com.fiap.fast_food_kitchen.controller;
 
 import br.com.fiap.fast_food_kitchen.dto.DemandKitchenRequest;
+import br.com.fiap.fast_food_kitchen.dto.DemandKitchenResponse;
 import br.com.fiap.fast_food_kitchen.dto.ProductKitchenRequest;
 import br.com.fiap.fast_food_kitchen.enums.DemandStatus;
 import br.com.fiap.fast_food_kitchen.usecase.IKitchenUsecase;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 class KitchenControllerTest {
 
@@ -39,6 +44,19 @@ class KitchenControllerTest {
 
         // Then
         verify(kitchenUsecase).saveDemand(request);
+    }
+
+    @Test
+    void shouldGetAllDemandsSuccessfully() {
+        // Given
+        List<DemandKitchenResponse> products = new ArrayList<>();
+
+        // When
+        when(kitchenUsecase.getReadyDemands()).thenReturn(products);
+        var demands = kitchenController.getAllDemands();
+
+        // Then
+        Assertions.assertNotNull(demands.getBody());
     }
 
 }
